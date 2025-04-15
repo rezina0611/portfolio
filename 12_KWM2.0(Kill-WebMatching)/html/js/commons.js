f5b4c90347ba421b4e2b33bbd45db5134be8a172
rezina0611 <han06110@naver.com>
2025-04-15 20:06:45 +0900

    Add files via upload

---
+++ 12_KWM2.0(Kill-WebMatching)/html/js/commons.js
@@ -0,0 +1,4 @@
+// production
+window.MAPBOX_TOKEN = 'pk.eyJ1IjoiZnVuemluLW1hcGJveCIsImEiOiJjbGg3Ymg2bDMwZTVpM2RtcHVwcWI0ZGdnIn0.TkCdokMyRSYYYAeQgTf6IA';
+
+window.CHAT_HOST = 'https://ckwm.funzin.co.kr';
