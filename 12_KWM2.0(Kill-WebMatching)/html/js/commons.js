73b12205b0d0bab626ad215420424f0008e4a14c
rezina0611 <han06110@naver.com>
2025-04-15 20:09:59 +0900

    Delete 12_KWM2.0(Kill-WebMatching) directory

--- 12_KWM2.0(Kill-WebMatching)/html/js/commons.js
+++
@@ -1,4 +0,0 @@
-// production
-window.MAPBOX_TOKEN = 'pk.eyJ1IjoiZnVuemluLW1hcGJveCIsImEiOiJjbGg3Ymg2bDMwZTVpM2RtcHVwcWI0ZGdnIn0.TkCdokMyRSYYYAeQgTf6IA';
-
-window.CHAT_HOST = 'https://ckwm.funzin.co.kr';
